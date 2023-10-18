6fe7d4b4479a497dfe8ff4d8619e3d5f074d1ec5
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-10-18 20:14:08 +0200

    Start project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App: React.FC<{}> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, odit
+        asperiores sunt facilis voluptatem veniam et repudiandae consequuntur
+        error excepturi? Hic ad pariatur reiciendis facere voluptatem, excepturi
+        laborum fugiat nemo.
+      </p>
+    </>
   );
-}
+};
 
 export default App;
