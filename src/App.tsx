4e21105a8f3fe0bb65a7a4c02c66a0e8c2cdd93c
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-10-18 20:29:36 +0200

    Added home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "pages/HomePage";
+
 const App: React.FC<{}> = () => {
   return (
-    <>
-      <h1>Hello world</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, odit
-        asperiores sunt facilis voluptatem veniam et repudiandae consequuntur
-        error excepturi? Hic ad pariatur reiciendis facere voluptatem, excepturi
-        laborum fugiat nemo.
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 };
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage: React.FC<{}> = () => {
+  return <p>Home page</p>;
+};
+
+export default HomePage;
