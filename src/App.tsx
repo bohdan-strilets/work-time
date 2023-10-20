b9832dd5c80efa836ff4ad9d8531716267d71ad0
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-10-20 05:10:23 +0200

    Added auth page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "components/Layout";
 import HomePage from "pages/HomePage";
+import AuthPage from "pages/AuthPage";
 
 const App: React.FC<{}> = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,5 @@
+const AuthPage: React.FC<{}> = () => {
+  return null;
+};
+
+export default AuthPage;
