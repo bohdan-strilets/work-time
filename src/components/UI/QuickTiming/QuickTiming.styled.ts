498b571023b33bb343cf208691a2ce16dc03818f
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-12-11 14:24:30 +0100

    Fixed responsiv design for QuickTiming component.

--- src/components/UI/QuickTiming/QuickTiming.styled.ts
+++ src/components/UI/QuickTiming/QuickTiming.styled.ts
@@ -1,23 +1,33 @@
 import styled from '@emotion/styled';
 import { ListProps } from 'types/props/QuickTimingProps';
+import ScreenWidth from 'utilities/ScreenWidth';
 
 export const List = styled.ul<ListProps>`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-wrap: wrap;
   margin: ${({ margin }) => (margin ? margin : '')};
+
+  @media screen and (min-width: ${ScreenWidth.tablet}) {
+    flex-wrap: nowrap;
+  }
 `;
 
 export const Item = styled.li`
-  margin-right: var(--small-indent);
+  width: calc((100% - 4 * 10px) / 2);
+  margin: 10px;
 
-  :last-child {
-    margin-right: 0;
+  @media screen and (min-width: ${ScreenWidth.tablet}) {
+    margin-right: var(--small-indent);
+    :last-child {
+      margin-right: 0;
+    }
   }
 `;
 
 export const Button = styled.button`
-  width: 80px;
+  width: 100%;
   height: 25px;
 
   background-color: var(--gray-color);
@@ -31,5 +41,9 @@ export const Button = styled.button`
   :hover,
   :focus {
     background-color: var(--accent-color);
+  }
+
+  @media screen and (min-width: ${ScreenWidth.tablet}) {
+    width: 80px;
   }
 `;
