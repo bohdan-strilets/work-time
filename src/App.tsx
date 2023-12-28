9cf1dd4351987e2d53c4674f223eb3d0002f0d54
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-12-28 18:07:45 +0100

    Added lazy load for routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from 'components/Layout';
-import HomePage from 'pages/HomePage';
-import AuthPage from 'pages/AuthPage';
-import CalendarPage from 'pages/CalendarPage';
-import ProfilePage from 'pages/ProfilePage';
-import AboutPage from 'pages/AboutPage';
-import ResetPasswordPage from 'pages/ResetPasswordPage';
-import StatisticsPage from 'pages/StatisticsPage';
+import Loader from 'components/UI/Loader';
 import useRefresh from 'hooks/useRefresh';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const AuthPage = lazy(() => import('pages/AuthPage'));
+const CalendarPage = lazy(() => import('pages/CalendarPage'));
+const ProfilePage = lazy(() => import('pages/ProfilePage'));
+const AboutPage = lazy(() => import('pages/AboutPage'));
+const ResetPasswordPage = lazy(() => import('pages/ResetPasswordPage'));
+const StatisticsPage = lazy(() => import('pages/StatisticsPage'));
 
 const App: React.FC<{}> = () => {
   const { isRefreshing } = useRefresh();
@@ -16,15 +19,17 @@ const App: React.FC<{}> = () => {
     <>
       {!isRefreshing ? (
         <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route path="/calendar" element={<CalendarPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/about-us" element={<AboutPage />} />
-            <Route path="/auth/reset-password" element={<ResetPasswordPage />} />
-            <Route path="/statistics" element={<StatisticsPage />} />
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/calendar" element={<CalendarPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/about-us" element={<AboutPage />} />
+              <Route path="/auth/reset-password" element={<ResetPasswordPage />} />
+              <Route path="/statistics" element={<StatisticsPage />} />
+            </Routes>
+          </Suspense>
         </Layout>
       ) : null}
     </>
