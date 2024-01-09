fabb523be8e159ee9e9287e985568df4a7f46b77
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-01-09 15:38:59 +0100

    Added sound when clicking the mouse.

---
+++ src/Assets/sounds/sprite.ts
@@ -0,0 +1,8 @@
+import { SpriteType } from 'types/types/SpriteType';
+import { SoundNamesEnum } from 'types/enums/SoundNamesEnum';
+
+const sprite: SpriteType = {
+  [SoundNamesEnum.MouseClick]: [0, 91.33786848072563],
+};
+
+export default sprite;
