6e4c2ee9c883ae4e5cee31e50fbde5c4ea67457c
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-02-05 11:31:57 +0100

    Fixed label on day cell.

--- src/components/Calendar/CellInformation/Responsiv/Desktop.tsx
+++ src/components/Calendar/CellInformation/Responsiv/Desktop.tsx
@@ -1,9 +1,12 @@
+import { useTranslation } from 'react-i18next';
 import { MdOutlineWork } from 'react-icons/md';
 import { BiSolidTimeFive } from 'react-icons/bi';
 import { BsCheckAll } from 'react-icons/bs';
 import { CellInformationProps } from 'types/props/CellInformationProps';
 import { Status } from 'types/enums/StatusEnum';
 import { WorkShiftNumber } from 'types/enums/WorkShiftNumber';
+import { CommonLngKeys } from 'types/locales/CommonLngKeys';
+import { LocalesKeys } from 'types/enums/LocalesKeys';
 import {
   DayOfMonth,
   Container,
@@ -15,15 +18,25 @@ import {
 } from '../CellInformation.styled';
 
 const Desktop: React.FC<CellInformationProps> = ({ dayInfo, date }) => {
+  const { t } = useTranslation();
+
   return (
     <>
       <DayOfMonth>{date.getDate()}</DayOfMonth>
       {dayInfo?.status && (
         <Container>
-          {dayInfo?.status === Status.work && <DayStatus>Work</DayStatus>}
-          {dayInfo?.status === Status.dayOff && <DayStatus>Day off</DayStatus>}
-          {dayInfo?.status === Status.vacation && <DayStatus>Vacation</DayStatus>}
-          {dayInfo?.status === Status.sickLeave && <DayStatus>Sick leave</DayStatus>}
+          {dayInfo?.status === Status.work && (
+            <DayStatus>{t(CommonLngKeys.Work, { ns: LocalesKeys.common })}</DayStatus>
+          )}
+          {dayInfo?.status === Status.dayOff && (
+            <DayStatus>{t(CommonLngKeys.DayOff, { ns: LocalesKeys.common })}</DayStatus>
+          )}
+          {dayInfo?.status === Status.vacation && (
+            <DayStatus>{t(CommonLngKeys.Vacation, { ns: LocalesKeys.common })}</DayStatus>
+          )}
+          {dayInfo?.status === Status.sickLeave && (
+            <DayStatus>{t(CommonLngKeys.SickLeave, { ns: LocalesKeys.common })}</DayStatus>
+          )}
           {dayInfo.workShiftNumber === WorkShiftNumber.Shift1 && (
             <LabelWrapper>
               <MdOutlineWork />
