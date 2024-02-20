969882190d7040bfee91c3b5e0945c497c15f29d
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-02-20 05:01:28 +0100

    Implemented the Google registration logic into a custom hook.

--- src/components/UI/GoogleAuthBtn/GoogleAuthBtn.tsx
+++ src/components/UI/GoogleAuthBtn/GoogleAuthBtn.tsx
@@ -1,43 +1,18 @@
-import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { GoogleLogin } from '@react-oauth/google';
 import { GoogleAuthBtnProps } from 'types/props/GoogleAuthBtnProps';
-import { useAppDispatch } from 'hooks/useAppDispatch';
-import operations from '../../../redux/user/userOperations';
-import { UserResponseType } from 'types/types/UserResponseType';
-import { UserType } from 'types/types/UserType';
-import useLocalStorage from 'hooks/useLocalStorage';
+import useGoogleAuth from 'hooks/useGoogleAuth';
 
 const GoogleAuthBtn: React.FC<GoogleAuthBtnProps> = ({ width }) => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const { setDataToLs } = useLocalStorage();
-
-  const handleSuccess = async (credentialResponse: CredentialResponse) => {
-    const token = credentialResponse.credential;
-    if (token) {
-      setDataToLs('google-token', token);
-      const response = await dispatch(operations.googleAuth(token));
-      const data = response.payload as UserResponseType<UserType>;
-      if (data && data.success) {
-        navigate('/calendar');
-      } else {
-        navigate('/auth');
-        toast.warning('Something went wrong, please check the entered data and try again.');
-      }
-    }
-  };
-
-  const handleError = () => {
-    toast.warning('Login failed');
-  };
+  const { handleError, handleSuccess } = useGoogleAuth();
 
   return (
-    <GoogleLogin
-      onSuccess={credentialResponse => handleSuccess(credentialResponse)}
-      onError={handleError}
-      width={width}
-    />
+    <>
+      <GoogleLogin
+        onSuccess={credentialResponse => handleSuccess(credentialResponse)}
+        onError={handleError}
+        width={width}
+      />
+    </>
   );
 };
 
---
+++ src/hooks/useGoogleAuth.ts
@@ -0,0 +1,52 @@
+import { CredentialResponse } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import axios, { AxiosError } from 'axios';
+import { useTranslation } from 'react-i18next';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+import operations from '../redux/user/userOperations';
+import { UserResponseType } from 'types/types/UserResponseType';
+import useLocalStorage from 'hooks/useLocalStorage';
+import CustomErrorHandler from 'utilities/CustomErrorHandler';
+import { LocalesKeys } from 'types/enums/LocalesKeys';
+import { ErrorLngKeys } from 'types/locales/ErrorsLngKeys';
+
+const useGoogleAuth = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { setDataToLs } = useLocalStorage();
+  const { t } = useTranslation();
+
+  const handleSuccess = async (credentialResponse: CredentialResponse) => {
+    const token = credentialResponse.credential;
+    if (token) {
+      setDataToLs('google-token', token);
+      await dispatch(operations.googleAuth(token));
+      try {
+        navigate('/calendar');
+      } catch (error: any) {
+        navigate('/auth');
+        if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError<UserResponseType>;
+          if (axiosError.response) {
+            const serverError = axiosError.response.data as UserResponseType;
+            CustomErrorHandler(serverError);
+          } else {
+            toast.error(t(ErrorLngKeys.GeneralAxiosError, { ns: LocalesKeys.error }));
+          }
+        } else {
+          toast.error(t(ErrorLngKeys.GeneralError, { ns: LocalesKeys.error }));
+        }
+      }
+    }
+  };
+
+  const handleError = () => {
+    toast.error(t(ErrorLngKeys.GeneralError, { ns: LocalesKeys.error }));
+    navigate('/calendar');
+  };
+
+  return { handleSuccess, handleError };
+};
+
+export default useGoogleAuth;
